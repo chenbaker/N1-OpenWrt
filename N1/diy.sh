1119e8c171cc56af9bff136f856516525f964e36
chenbaker <chenbaker@users.noreply.github.com>
2024-08-17 13:54:07 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -12,3 +12,4 @@ rm -rf package/helloworld
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
