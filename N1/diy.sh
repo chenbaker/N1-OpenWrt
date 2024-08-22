bfd4e2a6ee839ee619ac646659384b04f6106495
chenbaker <chenbaker@users.noreply.github.com>
2024-08-23 07:14:35 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.200/g' package/base-files/files/bin/config_generate
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -11,5 +11,5 @@ rm -rf package/helloworld
 # Add packages
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
